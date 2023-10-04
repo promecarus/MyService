cb02829d0359b5101d5cb37655497618325a2ca2
promecarus <haikalslipi@gmail.com>
2023-10-04 11:05:44 +0700

    part01

--- app/src/main/java/com/promecarus/myservice/MainActivity.kt
+++ app/src/main/java/com/promecarus/myservice/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.promecarus.myservice
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.promecarus.myservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val serviceIntent = Intent(this, MyBackgroundService::class.java)
+        binding.btnStartBackgroundService.setOnClickListener { startService(serviceIntent) }
+        binding.btnStopBackgroundService.setOnClickListener { stopService(serviceIntent) }
     }
 }
