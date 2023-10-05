f44d9cd30d4d3c4795a085b33c9c264bd60f8c80
promecarus <haikalslipi@gmail.com>
2023-10-05 09:35:03 +0700

    part02

--- app/src/main/java/com/promecarus/myservice/MainActivity.kt
+++ app/src/main/java/com/promecarus/myservice/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.promecarus.myservice
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.promecarus.myservice.databinding.ActivityMainBinding
@@ -16,5 +17,18 @@ class MainActivity : AppCompatActivity() {
         val serviceIntent = Intent(this, MyBackgroundService::class.java)
         binding.btnStartBackgroundService.setOnClickListener { startService(serviceIntent) }
         binding.btnStopBackgroundService.setOnClickListener { stopService(serviceIntent) }
+
+        val foregroundServiceIntent = Intent(this, MyForegroundService::class.java)
+        binding.btnStartForegroundService.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= 26) {
+                startForegroundService(foregroundServiceIntent)
+            } else {
+                startService(foregroundServiceIntent)
+            }
+        }
+        binding.btnStopForegroundService.setOnClickListener {
+            stopService(foregroundServiceIntent)
+        }
+
     }
 }
